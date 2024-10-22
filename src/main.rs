6bb5bd8b29f00daed2799ebc59268be7b4495039
meraghon <163075785+meraghon@users.noreply.github.com>
2024-10-22 12:48:16 -0400

    Framework for writing hashes to baseline.txt

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::ascii;
 use sha2::{Sha512, Digest};
 use std::fs;
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::Write;
+use std::io::{BufReader, Read, BufWriter};
 use std::path::Path;
 
 fn sha512_hash(path: &Path) -> Result<String, io::Error> {
@@ -28,15 +29,6 @@ fn sha512_hash(path: &Path) -> Result<String, io::Error> {
 
 fn main() {
 
-    //test file hashing
-    let path = Path::new("C:/Users/Morrighan/OneDrive/Desktop/FIM/files/file1.txt");
-    match sha512_hash(path) {
-        Ok(hash) => println!("SHA512 Hash: {hash}"),
-        Err(e) => eprintln!("Error: {e}"),
-    } 
-    
-
-
     println!("A) Collect New Baseline?");
 
     println!("B)  Monitor Files w/ Saved Baseline.");
@@ -54,10 +46,10 @@ fn main() {
 
     if input == "A" {
 
-        println!("Collect baseline. Input folder path containing the files to test:");
-        //Have user input the path of folder with the files to test
-        //Testing path : C:\Users\Morrighan\Desktop\FIM Files\Files
+        // Prompt user to input the absolute path of the folder containing the files
+        println!("Collect baseline. Input absolute folder path containing the files to test:");
 
+        // Create variable to store the folder path
         let mut file_location = String::new();
 
         io::stdin()
@@ -68,6 +60,15 @@ fn main() {
 
         
         let path = Path::new(file_location);
+
+        // Input folder where the baseline.txt file is to be stored 
+        let folder_path = "C:/Users/mdayt/Documents/FIM Project/Baseline Output";
+        let baseline_path = format!("{}/baseline.txt", folder_path);
+
+        // Create the baseline.txt file
+        let mut baseline = File::create(&baseline_path).expect("Failed to create file");
+        // Writes to baseline
+        let mut writer = BufWriter::new(baseline);
         
         
         //Check if path provided is a directory
@@ -77,23 +78,31 @@ fn main() {
 
                 Ok(entries) => {
 
+                    // Iterates over each file in the directory
                     for entry in entries {
 
                         match entry {
 
                             Ok(entry) => {
 
-                                //Prints full paths of files within the folder
-                                //println!("{}",entry.path().display());
-
                                 let filepath = entry.path();
-
-                                println!("File path: {} ", filepath.display());
                                 
                                 match sha512_hash(&filepath) {
-                                    Ok(hash) => println!("SHA512 Hash: {hash}"),
-                                    Err(e) => eprintln!("Error: {e}"),
+        
+                                    Ok(hash) => {
+                                        // Writes file path and hash to the baseline.txt file
+                                        let output = format!("File path: {}\nSHA512 Hash: {hash}\n", path.display());
+                                        writer.write_all(output.as_bytes()).expect("Failed to write to file.");
+                                    }
+                            
+                                    Err(e) => {
+                                        let error_message = format!("Error: {e}");
+                                        writer.write_all(error_message.as_bytes()).expect("Failed to write to file.");
+                                    }
                                 } 
+
+                                // Ensure data is flushed to disk
+                                writer.flush().expect("Failed to flush buffer");
                                 
                             }
                             Err(e) => {
@@ -120,6 +129,8 @@ fn main() {
     
     else if input == "B" {
         println!("Monitor");
+        // Go through and re-hash all files within the folder
+        // Compare them to what is in the baseline.txt
     }
    else {
         println!("Process terminated. Must input A or B.");
