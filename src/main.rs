1774ba669e0ddcdb6866638839d3cb25d694abd8
meraghon <163075785+meraghon@users.noreply.github.com>
2024-10-23 10:41:22 -0400

    Prompt user to input location for baseline.txt

--- src/main.rs
+++ src/main.rs
@@ -51,18 +51,23 @@ fn main() {
 
         // Create variable to store the folder path
         let mut file_location = String::new();
-
         io::stdin()
             .read_line(&mut file_location)
             .expect("Failed to readline");
-        
         let file_location = file_location.trim();
 
-        
+        // File location for testing: C:/Users/mdayt/Documents/FIM Project/testfiles
         let path = Path::new(file_location);
 
-        // Input folder where the baseline.txt file is to be stored 
-        let folder_path = "C:/Users/mdayt/Documents/FIM Project/Baseline Output";
+        println!("Input absolute folder path to store the baseline file");
+        // Input folder where the baseline.txt file is to be stored     
+        let mut folder_path = String::new();
+        io::stdin()
+            .read_line(&mut folder_path)
+            .expect("Failed to read line");
+        let folder_path = folder_path.trim();
+        // Folder path for testing : C:/Users/mdayt/Documents/FIM Project/Baseline Output
+
         let baseline_path = format!("{}/baseline.txt", folder_path);
 
         // Create the baseline.txt file
@@ -99,7 +104,7 @@ fn main() {
                                         let error_message = format!("Error: {e}");
                                         writer.write_all(error_message.as_bytes()).expect("Failed to write to file.");
                                     }
-                                } 
+                                }
 
                                 // Ensure data is flushed to disk
                                 writer.flush().expect("Failed to flush buffer");
@@ -123,12 +128,15 @@ fn main() {
         } else  {
             eprint!("error");
         }
+        // Confirmation of hashes written to file
+        println!("Hashes successfully written to baseline.txt");
 
     }
 
     
     else if input == "B" {
         println!("Monitor");
+        // Check if baseline.txt exists, if it exists continue if not then end program?
         // Go through and re-hash all files within the folder
         // Compare them to what is in the baseline.txt
     }
